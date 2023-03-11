b0014300ebfd31b5f2defc4db2bb2fcda88feb7c
wbfw109 <wbfw109@gmail.com>
2023-03-11 09:12:44 +0900

    update minor points.

--- src/python/wbfw109/algorithms/baekjoon/gold_temp.py
+++ src/python/wbfw109/algorithms/baekjoon/gold_temp.py
@@ -49,7 +49,9 @@ def hang_balloons_to_teams(input_lines: Optional[Iterator[str]] = None) -> str:
         Result of the cases that the either queue (A or B) is prioritized than another queue is different.
     -> use Two heapq to save opportunity cost.
 
-    ... 알수가없음..
+    여기서 기회비용은 abs(A 와 떨어진 거리 - B 와 떨어진 거리) 이다.
+
+    //''';;;각 테스트케이스에 대해서...
 
     """
     import dataclasses
@@ -63,8 +65,8 @@ def hang_balloons_to_teams(input_lines: Optional[Iterator[str]] = None) -> str:
 
     @dataclasses.dataclass(eq=True, order=True)
     class Team:
-        distance_1: int
-        distance_2: int
+        distance_a: int
+        distance_b: int
         required_balloons: int
 
     # Title: input
@@ -73,71 +75,55 @@ def hang_balloons_to_teams(input_lines: Optional[Iterator[str]] = None) -> str:
     # 🚣 condition (Σ(required_balloons) ≤ A+B)
     n, a, b = list(map(int, input_().split()))
     given_balloons: list[int] = [a, b]
+
     # condition (0 ≤  distance apart from (A, B)  ≤ 10^3)
-    a_heapq: list[Team] = []
-    b_heapq: list[Team] = []
-    # why n+1? ignore last line ("0 0 0")
-    for _ in range(n + 1):
+    team_heapq: list[tuple[int, Team]] = []
+    for _ in range(n):
         line: list[int] = list(map(int, input_().split()))
 
         # if required balloons is none, it is not need to process.
         if line[0] == 0:
             continue
 
-        if line[1] < line[2]:
-            a_heapq.append(Team(line[1], -line[2], required_balloons=line[0]))
-        else:
-            b_heapq.append(Team(line[2], -line[1], required_balloons=line[0]))
+        team_heapq.append(
+            (-abs(line[1] - line[2]), Team(line[1], line[2], required_balloons=line[0]))
+        )
     minimum_sum_of_distance: int = 0
 
     # Title: solve
-    heapq.heapify(a_heapq)
-    heapq.heapify(b_heapq)
-    a_b_pointer: int = 0
-    heapq_list: list[list[Team]] = [a_heapq, b_heapq]
+    a_b_pointer: int = -1
+    heapq.heapify(team_heapq)
 
     # this loop must be run at least one time according to given conditions.
-    while True:
-        match bool(a_heapq), bool(b_heapq):
-            case True, True:
-                if a_heapq[0].distance_1 < b_heapq[0].distance_1:
-                    target_team: Team = a_heapq[0]
-                    a_b_pointer = 0
-                else:
-                    target_team: Team = b_heapq[0]
-                    a_b_pointer = 1
-            case False, True:
-                target_team: Team = b_heapq[0]
-                a_b_pointer = 1
-            case True, False:
-                target_team: Team = a_heapq[0]
-                a_b_pointer = 0
-            case _:
-                break
+    while team_heapq:
+        _, team = team_heapq[0]
+        if team.distance_a < team.distance_b:
+            a_b_pointer = 0
+            is_a: bool = True
+        else:
+            a_b_pointer = 1
+            is_a: bool = False
 
         if given_balloons[a_b_pointer] != 0:
-            used_balloons = min(
-                given_balloons[a_b_pointer], target_team.required_balloons
-            )
+            used_balloons = min(given_balloons[a_b_pointer], team.required_balloons)
             given_balloons[a_b_pointer] -= used_balloons
-            target_team.required_balloons -= used_balloons
-            minimum_sum_of_distance += used_balloons * target_team.distance_1
-            if target_team.required_balloons == 0:
-                heapq.heappop(heapq_list[a_b_pointer])
+            team.required_balloons -= used_balloons
+            if is_a:
+                minimum_sum_of_distance += used_balloons * team.distance_a
+            else:
+                minimum_sum_of_distance += used_balloons * team.distance_b
+            if team.required_balloons == 0:
+                heapq.heappop(team_heapq)
         else:
-            minimum_sum_of_distance += sum(
-                (
-                    -team.distance_2 * team.required_balloons
-                    for team in heapq_list[a_b_pointer]
+            # reverse operation since previous <given_balloons[a_b_pointer]> is 0
+            if is_a:
+                minimum_sum_of_distance += sum(
+                    (team.distance_b * team.required_balloons for _, team in team_heapq)
                 )
-            )
-            a_b_pointer = (a_b_pointer + 1) % 2
-            minimum_sum_of_distance += sum(
-                (
-                    team.distance_1 * team.required_balloons
-                    for team in heapq_list[a_b_pointer]
+            else:
+                minimum_sum_of_distance += sum(
+                    (team.distance_a * team.required_balloons for _, team in team_heapq)
                 )
-            )
             break
 
     # Title: output
@@ -145,41 +131,48 @@ def hang_balloons_to_teams(input_lines: Optional[Iterator[str]] = None) -> str:
     return str(minimum_sum_of_distance)
 
 
-# 우선순위가 변위가 큰 것 abs(a-b)
 def test_hang_balloons_to_teams() -> None:
     test_case = unittest.TestCase()
     for input_lines, output_lines in [
         [
             [
-                "3 15 50",
-                "10 1 2",
-                "10 2 3",
-                "10 4 1000",
+                "1 5 5",
+                "10 2 1",
                 "0 0 0",
             ],
-            ["30"],
+            ["15"],
         ],
-        # [
-        #     [
-        #         "3 15 35",
-        #         "10 20 10",
-        #         "10 10 30",
-        #         "10 40 10",
-        #         "0 0 0",
-        #     ],
-        #     ["300"],
-        # ],
-        # [
-        #     [
-        #         "4 25 25",
-        #         "10 20 10",
-        #         "10 10 30",
-        #         "10 30 15",
-        #         "10 40 20",
-        #         "0 0 0",
-        #     ],
-        #     ["650"],
-        # ],
+        [
+            [
+                "3 15 50",
+                "10 1 2",  #  20   |  5+10
+                "10 2 3",  # 10+15 |  30
+                "10 4 1000",  # 40
+                "0 0 0",
+            ],
+            ["85"],
+        ],
+        [
+            [
+                "3 15 35",
+                "10 20 10",
+                "10 10 30",
+                "10 40 10",
+                "0 0 0",
+            ],
+            ["300"],
+        ],
+        [
+            [
+                "4 25 25",
+                "10 20 10",  # 50+100
+                "10 10 30",  # 100
+                "10 30 15",  # 150
+                "10 40 20",  # 200
+                "0 0 0",
+            ],
+            ["600"],
+        ],
     ]:
         start_time = time.time()
         test_case.assertEqual(
