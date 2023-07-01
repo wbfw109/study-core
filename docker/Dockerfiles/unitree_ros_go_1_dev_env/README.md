383861c5f62c9a32c2c2b62c69bd860b764f949f
wbfw109 <wbfw109@gmail.com>
2023-07-02 04:23:12 +0900

    update minor points.

--- docker/Dockerfiles/unitree_ros_go_1_dev_env/README.md
+++ docker/Dockerfiles/unitree_ros_go_1_dev_env/README.md
@@ -1,5 +1,56 @@
 # README
 
+- [README](#readme)
+  - [1. How to make the Development environment for Unitree ROS 1 (Melodic)?](#1-how-to-make-the-development-environment-for-unitree-ros-1-melodic)
+    - [1.1. use pre-made Docker image](#11-use-pre-made-docker-image)
+      - [1.1.1. How to run commands?](#111-how-to-run-commands)
+      - [1.1.2. How to display GUI by commands?](#112-how-to-display-gui-by-commands)
+    - [1.2. build Docker image yourself](#12-build-docker-image-yourself)
+
 🎉 Welcome to Unitree ROS 1 (Melodic) Development environment
 
-➡️ Test commands in 🔗 https://github.com/unitreerobotics/unitree_ros#detail-of-packages
+➡️ [Test commands](https://github.com/unitreerobotics/unitree_ros#detail-of-packages)
+
+## 1. How to make the Development environment for Unitree ROS 1 (Melodic)?
+
+### 1.1. use [pre-made Docker image](https://hub.docker.com/repository/docker/wbfw109/unitree_ros_go_1_dev_env/general)
+
+#### 1.1.1. How to run commands?
+
+This Docker image is written as **root user**.
+so, you need to change user by `su --login`.
+
+#### 1.1.2. How to display GUI by commands?
+
+If you run in headless environment,
+
+- use communication protocol that specifies the communication between a display server and its clients like Wayland, X11.
+- ⭕ **(Recommended)** or, you may use `>Dev Containers: Reopen in Container` command in VS Code with `.devcontainer/devcontainer.json`
+
+  - ```json
+    {
+      "name": "Unitree ROS 1 (Melodic) Development environment",
+      "image": "wbfw109/unitree_ros_go_1_dev_env:latest",
+      "remoteUser": "root"
+    }
+    ```
+
+  - in this case, it is already set for remote user as **root**, so you do not have to run `su --login`.
+
+### 1.2. build Docker image yourself
+
+📝 If your computer architecture is different with **linux/amd64 (pre-made image's architecture**), you have to the build image yourself.
+
+for example, in VSCode
+
+- use VS Code and run `>Dev Containers: Reopen in Container` command in VS Code with `.devcontainer/devcontainer.json`
+
+  - ```json
+    {
+      "name": "Ubuntu",
+      "image": "mcr.microsoft.com/devcontainers/cpp:ubuntu-18.04"
+    }
+    ```
+
+- run [setting_unitree_go_1.sh](setting_unitree_go_1.sh)
+  - It is installed as normal **user**, not root.
--- docker/Dockerfiles/unitree_ros_go_1_dev_env/setting_unitree_go_1.sh
+++ docker/Dockerfiles/unitree_ros_go_1_dev_env/setting_unitree_go_1.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+### Note that If you use WSL2, add Google public domain (8.8.8.8 and 8.8.4.4) into /etc/resolv.conf.
+#   otherwise, when some `apt install` packages, you may get Error mesage `Failed to fetch ... Connection failed ... E: Unable to fetch some archives, maybe run apt-get update or try with --fix-missing?`.
+# This file can be called after run ">Dev Containers: Reopen in Container" in VS Code, so that the container will work well.
+
 ### ROS project Commands
 ## from
 # https://github.com/unitreerobotics/unitree_ros
@@ -5,10 +10,8 @@
 # - https://github.com/unitreerobotics/unitree_ros_to_real
 #   - https://github.com/unitreerobotics/unitree_legged_sdk
 
-# install ROS 1 Melodic
-working_dir="$PWD"
-echo "$working_dir"
 
+# install ROS 1 Melodic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
@@ -29,19 +32,19 @@ curl --silent --show-error --location -o file.zip https://github.com/unitreerobo
     unzip file.zip && rm file.zip && mv unitree_ros-master unitree_ros
 curl --silent --show-error --location -o file.zip https://github.com/unitreerobotics/unitree_legged_sdk/archive/go1.zip && \
     unzip file.zip && rm file.zip && mv unitree_legged_sdk-go1 unitree_legged_sdk
-curl --silent --show-error --location -o file.zip http://github.com/unitreerobotics/unitree_ros_to_real/archive/master.zip && \
-    unzip file.zip && rm file.zip &&\
-    mv unitree_ros_to_real-master/unitree_legged_real unitree_ros_to_real &&\
-    mv unitree_ros_to_real-master/unitree_legged_msgs unitree_legged_msgs &&\
+curl --silent --show-error --location -o file.zip https://github.com/unitreerobotics/unitree_ros_to_real/archive/master.zip && \
+    unzip file.zip && rm file.zip && \
+    mv unitree_ros_to_real-master/unitree_legged_real unitree_legged_real && \
+    mv unitree_ros_to_real-master/unitree_legged_msgs unitree_legged_msgs && \
     rm -r unitree_ros_to_real-master
 
 # pro-process for unitree_ros repository
-sudo apt install -y ros-melodic-controller-interface  ros-melodic-gazebo-ros-control ros-melodic-joint-state-controller ros-melodic-effort-controllers ros-melodic-joint-trajectory-controller
+sudo apt install -y ros-melodic-controller-interface ros-melodic-gazebo-ros-control ros-melodic-joint-state-controller ros-melodic-effort-controllers ros-melodic-joint-trajectory-controller
 sed -i "s#/home/unitree/catkin_ws/src#$PWD#g" unitree_ros/unitree_gazebo/worlds/stairs.world
 
 ### Build
 # build <unitree_legged_sdk>
-cd unitree_legged_sdk && mkdir build && cd build && cmake ../ && make
+mkdir unitree_legged_sdk/build && cd unitree_legged_sdk/build && cmake ../ && make
 echo "source $PWD/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
@@ -54,12 +57,3 @@ source ~/.bashrc
 sudo apt install -y ros-melodic-joint-state-publisher-gui
 
 ## ➡️ test commands! from 🔗 https://github.com/unitreerobotics/unitree_ros#detail-of-packages
-
-
-
-### deprecated (no longer used)
-# curl https://github.com/unitreerobotics/unitree_legged_sdk/archive/refs/tags/v3.8.0.zip -Ss -L -o file.zip && \
-#     unzip file.zip && rm file.zip && mv unitree_legged_sdk-3.8.0 unitree_legged_sdk
-
-### Note that If you use WSL2, add Google public domain (8.8.8.8 and 8.8.4.4) into /etc/resolv.conf.
-# otherwise, when some `apt install` packages, you may get Error mesage `Failed to fetch ... Connection failed ... E: Unable to fetch some archives, maybe run apt-get update or try with --fix-missing?`.
